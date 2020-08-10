2d20c0784aa59489782abff81a8440b9406bfd2b
occn8 <ochenwayward@gmail.com>
2020-08-10 13:49:37 +0300

    favcon

--- index.php
+++ index.php
@@ -72,13 +72,7 @@
             style="background-image: url(images/books-2.jpg);background-repeat: no-repeat;background-size: cover; height: 80vh;">
             <div class="container" style="background-color: beige;padding: 10px;">
                 <h1>Kabooja Boys School</h1>
-                <p class="lead text-muted">Something short and leading about the collection below—its contents, the
-                    creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it
-                    entirely.</p>
-                <p>
-                    <a href="#" class="btn btn-primary my-2">Main call to action</a>
-                    <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-                </p>
+                
             </div>
         </section>
 
--- login.php
+++ login.php
@@ -22,8 +22,10 @@
     <?php include('errors.php'); ?>
     <center>
       <img class="mb-4" src="images/books-1.jpg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">sign in</h1>
     </center>
-    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+    
+    <h1 class="h3 mb-3 font-weight-normal">Kabooja Boys School</h1>
     <label for="inputEmail" class="sr-only">Email address</label>
     <input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="email" required
       autofocus><br>
